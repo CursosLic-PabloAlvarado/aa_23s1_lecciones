32f37c27e406e6a64507a91deaaaca89f2d49460
Pablo Alvarado-Moya <palvaradomoya@gmail.com>
2022-02-22 11:42:33 -0600

    Mejores parámetros

--- lec05/cuadratico.m
+++ lec05/cuadratico.m
@@ -6,7 +6,7 @@ a = linspace(0,2*pi,360);
 X = [cos(a)+1;sin(a)+1];
 
 
-A=[2 0.1;0.1 2]; B=[-0.5 0];
+A=[2 0.1;0.1 1]; B=[-0.5 0];
 
 ## Evalúe eficientemente x' A x + B x  donde x es cada columna de X
  
